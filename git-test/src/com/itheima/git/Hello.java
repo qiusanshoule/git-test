64a02e46489964a93d0404aa8debc3eb3eb01a7d
zhangsan <zhangsan@itcast.cn>
2020-03-18 16:32:39 +0800

    将本地工程添加到git中

---
+++ git-test/src/com/itheima/git/Hello.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
