cbff06fce968b306f7400197d76df0abd23e7c96
zhangsan <zhangsan@itcast.cn>
2020-03-18 16:44:12 +0800

    修改内容

--- git-test/src/com/itheima/git/Hello.java
+++ git-test/src/com/itheima/git/Hello.java
@@ -6,10 +6,12 @@ public class Hello {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        //hello test
     }
 
     public void test(){
         int i=0;
         i += 10;
+        System.out.println("i = " + i);
     }
 }
